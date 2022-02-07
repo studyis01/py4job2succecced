ffed6e4b464e399272ecc0c7580e94e77a2724ff
胡锦博 <hujinbo@192.168.0.106>
2022-02-07 22:06:19 +0800

    error modifyed

--- python_algorithm/insert_sort_0205_1.py
+++ python_algorithm/insert_sort_0205_1.py
@@ -2,13 +2,21 @@ def insert_sort(list):
     length = len(list)
     for i in range(1, length):
         if list[i] < list[i - 1]:
-            index = i
             temp = list[i]
-        for j in range(i - 1, -1, -1):
-            if list[j] < temp:
-                list[j + 1] = list[j]
-                temp = j
-            else:
-                break
-        temp = list[j]
+            index = i
+            # 记得要缩进
+            for j in range(i - 1, -1, -1):
+                # 是 大于 不是 小于，和取出元素大的元素交换
+                if list[j] > temp:
+                    list[j + 1] = list[j]
+                    # 下标保存是 index，不是temp
+                    index = j
+                else:
+                    break
+            # list[index]不是list[j]
+            list[index] = temp
     return list
+
+if __name__ == '__main__':
+    list = [1, 10, 99, 87, 65, 4, 100]
+    print(insert_sort(list))
